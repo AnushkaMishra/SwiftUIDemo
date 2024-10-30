7c33e11fed3404cb80237381101c597b917b610c
anushka.mishra <anushkamishra296@gmail.com>
2024-10-30 23:00:39 +0530

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Anushka Mishra on 30/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
